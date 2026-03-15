e55902a7892b25ca0b0d5a2e44f48c1d1654c207
phucla <laducphuc@gmail.com>
2026-03-15 23:28:26 +0700

    Test

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
 
-const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const bot = new Telegraf(BOT_TOKEN);
